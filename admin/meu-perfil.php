333dfa2b3ea20b1e8e5b8d6c680f5ad10fbdcd97
plinioteixeira <plinio.t.vasconcellos@gmail.com>
2022-06-02 16:09:26 -0300

    Atualizar as contas

--- admin/meu-perfil.php
+++ admin/meu-perfil.php
@@ -1,5 +1,30 @@
 <?php 
 require "../inc/cabecalho-admin.php"; 
+require "../inc/funcoes-usuarios.php";
+
+// carregaremos os dados da pessoa logada na sessão ($_SESSION['id'])
+$usuario = lerUmUsuario($conexao, $_SESSION['id']);
+
+if(isset($_POST['atualizar'])){
+  $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
+  $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+  $tipo = $_SESSION['tipo'];
+  $id = $_SESSION['id'];
+
+  /* lógica para senha
+  se o campo da senha do formulário estiver vazio, então significa que o usuário NÃO MUDOU A SENHA. */
+  if (empty($_POST['senha'])) {
+    $senha = $usuario['senha'];
+  } else {
+  /* Caso contrário, se o usuário digitou alguma alguma coisa no campo senha, precisaremos verificar a senha digitada.  */
+  $senha = verificaSenha($_POST['senha'], $usuario['senha']);
+  }
+  /* echo $usuario['senha'];
+  echo "<br>";
+  echo "formulário".$senha; */
+  atualizarUsuario($conexao, $id, $nome, $email, $senha, $tipo);
+  header("location:index.php");
+}
 
 ?>
   <div class="row">
@@ -11,26 +36,17 @@ require "../inc/cabecalho-admin.php";
 
         <div class="form-group">
           <label for="nome">Nome:</label>
-          <input class="form-control" required type="text" id="nome" name="nome" placeholder="Nome (obrigatório)">
+          <input value="<?=$usuario['nome']?>" class="form-control" required type="text" id="nome" name="nome" placeholder="Nome (obrigatório)">
         </div>
         <div class="form-group">
           <label for="email">E-mail:</label>
-          <input class="form-control" required type="email" id="email" name="email" placeholder="E-mail (obrigatório)">
+          <input value="<?=$usuario['email']?>" class="form-control" required type="email" id="email" name="email" placeholder="E-mail (obrigatório)">
         </div>
         <div class="form-group">
           <label for="senha">Senha</label>
           <input class="form-control" type="password" id="senha" name="senha"  placeholder="Preencha apenas se for alterar">
         </div>
 
-        <!-- Select de tipos/níveis de usuários -->
-        <div class="form-group">
-          <label for="tipo">Tipo:</label>
-          <select name="tipo" id="tipo" required class="custom-select">
-              <option value=""></option>                    
-              <option value="editor">Editor</option>     
-              <option value="admin">Administrador</option>
-          </select>
-        </div>
 
         <button class="btn btn-lg btn-primary" name="atualizar">Atualizar</button>
       </form>
